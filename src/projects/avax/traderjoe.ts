e4320b3f723ace2e88fc30bc00e008349112d36c
Ncookiez <ncukiert@gmail.com>
2022-05-06 15:55:13 -0300

    Added Boosted Farms on Trader Joe (AVAX)

--- src/projects/avax/traderjoe.ts
+++ src/projects/avax/traderjoe.ts
@@ -10,6 +10,7 @@ const chain: Chain = 'avax';
 const project = 'traderjoe';
 const masterChefV2: Address = '0xd6a4F121CA35509aF06A0Be99093d08462f53052';
 const masterChefV3: Address = '0x188bED1968b795d5c9022F6a0bb5931Ac4c18F00';
+const boostedMasterChef: Address = '0x4483f0b6e2F5486D06958C20f8C39A7aBe87bf8F';
 const bankController: Address = '0xdc13687554205E5b89Ac783db14bb5bba4A1eDaC';
 const joe: Address = '0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd';
 const xjoe: Address = '0x57319d41F71E81F3c65F2a47CA4e001EbAFd4F33';
@@ -23,6 +24,7 @@ export const get = async (wallet: Address) => {
     balance.push(...(await getStakedJOE(wallet)));
     balance.push(...(await getFarmV2Balances(wallet)));
     balance.push(...(await getFarmV3Balances(wallet)));
+    balance.push(...(await getBoostedFarmBalances(wallet)));
     balance.push(...(await getMarketBalances(wallet)));
   } catch {
     console.error(`Error fetching ${project} balances on ${chain.toUpperCase()}.`);
@@ -144,6 +146,66 @@ export const getFarmV3Balances = async (wallet: Address) => {
   
         // JOE Rewards:
         let rewards = await query(chain, masterChefV3, traderjoe.masterChefABI, 'pendingTokens', [farmID, wallet]);
+        let pendingJoe = parseInt(rewards.pendingJoe);
+        if(pendingJoe > 0) {
+          let newToken = await addToken(chain, project, 'unclaimed', joe, pendingJoe, wallet);
+          balances.push(newToken);
+        }
+  
+        // Bonus Rewards:
+        let pendingBonus = parseInt(rewards.pendingBonusToken);
+        if(pendingBonus > 0) {
+          let newToken = await addToken(chain, project, 'unclaimed', rewards.bonusTokenAddress, pendingBonus, wallet);
+          balances.push(newToken);
+        }
+      }
+    }
+  })());
+  await Promise.all(promises);
+  return balances;
+}
+
+// Function to get boosted farm balances:
+export const getBoostedFarmBalances = async (wallet: Address) => {
+  let balances: (Token | LPToken | XToken)[] = [];
+  let farmCount = parseInt(await query(chain, boostedMasterChef, traderjoe.masterChefABI, 'poolLength', []));
+  let farms = [...Array(farmCount).keys()];
+  
+  // Multicall Query Setup:
+  let queries: ContractCallContext[] = [];
+  let balanceQuery: ContractCallContext = {
+    reference: 'userInfo',
+    contractAddress: boostedMasterChef,
+    abi: traderjoe.masterChefABI,
+    calls: []
+  }
+  farms.forEach(farmID => {
+    balanceQuery.calls.push({ reference: farmID.toString(), methodName: 'userInfo', methodParameters: [farmID, wallet] });
+  });
+  queries.push(balanceQuery);
+  
+  // Multicall Query Results:
+  let multicallResults = (await multicallQuery(chain, queries)).results;
+  let promises = multicallResults['userInfo'].callsReturnContext.map(result => (async () => {
+    if(result.success) {
+      let farmID = parseInt(result.reference);
+      let balance = parseBN(result.returnValues[0]);
+      if(balance > 0) {
+        let token = (await query(chain, boostedMasterChef, traderjoe.masterChefABI, 'poolInfo', [farmID])).lpToken;
+
+        // xJOE Farm:
+        if(token === xjoe) {
+          let newToken = await addTraderJoeToken(chain, project, 'staked', balance, wallet);
+          balances.push(newToken);
+  
+        // LP Farms:
+        } else {
+          let newToken = await addLPToken(chain, project, 'staked', token, balance, wallet);
+          balances.push(newToken);
+        }
+  
+        // JOE Rewards:
+        let rewards = await query(chain, boostedMasterChef, traderjoe.masterChefABI, 'pendingTokens', [farmID, wallet]);
         let pendingJoe = parseInt(rewards.pendingJoe);
         if(pendingJoe > 0) {
           let newToken = await addToken(chain, project, 'unclaimed', joe, pendingJoe, wallet);
